0f2194308a89da2cc4a74b0e60d8fbd4638cafa9
hytiek <56261680+hytiek@users.noreply.github.com>
2024-03-18 14:44:15 -0400

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,77 @@
+--[[
+    ____        _     ____  ____  ______ 
+   / __ \____ _(_)___/ / / / / / / / __ \
+  / /_/ / __ `/ / __  / /_/ / / / / / / /
+ / _, _/ /_/ / / /_/ / __  / /_/ / /_/ / 
+/_/ |_|\__,_/_/\__,_/_/ /_/\____/_____/  
+
+- from hytiek (version 1.0)
+Let's collaborate; submit PR to https://github.com/hytiek/raidhud
+
+]]
+--- @type Mq
+local mq = require('mq')
+--- @type ImGui
+require('ImGui')
+
+local terminate, isOpen, shouldDraw = false, true, true
+
+local Raids = require('raidhud.raid-list')
+
+local function updateRaidStatus()
+    for i, era in ipairs(Raids) do
+        for ii, event in ipairs(era.events) do
+            local eventIndex = mq.TLO.Window("DynamicZoneWnd/DZ_TimerList").List("=" .. event.name ..",3")()
+            if eventIndex ~= nil then
+                Raids[i].events[ii].available = false
+                Raids[i].events[ii].lockedout = tostring(mq.TLO.Window("DynamicZoneWnd/DZ_TimerList").List(eventIndex,1))
+            end
+        end
+    end
+end
+
+local function updateUI()
+    if not isOpen then return end
+    isOpen, shouldDraw = ImGui.Begin('RaidHUD', isOpen)
+    if shouldDraw then
+        for _, era in ipairs(Raids) do
+            if (ImGui.CollapsingHeader(era.era)) then
+                for _, event in ipairs(era.events) do
+                    if event.available == true then
+                        ImGui.BulletText(event.name);
+                        if ImGui.IsItemHovered() then
+                            ImGui.SetTooltip(event.tooltip);
+                        end
+                    else
+                        ImGui.Bullet()
+                        ImGui.TextDisabled(event.name);
+                        if ImGui.IsItemHovered() then
+                            ImGui.SetTooltip("Lockout time: " .. event.lockedout);
+                        end
+                    end
+                end
+            end
+        end
+    end
+    ImGui.End()
+end
+
+local function toggleUI()
+    isOpen = not isOpen
+end
+
+mq.imgui.init('RaidHUD', updateUI)
+mq.bind('/raidhud', toggleUI)
+
+-- open DZ window for a moment so we can fetch info
+while mq.TLO.Window("DynamicZoneWnd").Open() ~= true do
+    mq.TLO.Window("DynamicZoneWnd").DoOpen()
+    mq.delay(100)
+end
+mq.TLO.Window("DynamicZoneWnd").DoClose()
+updateRaidStatus()
+
+while not terminate and mq.TLO.MacroQuest.GameState() == "INGAME" do
+    mq.delay("60s", function() return terminate end)
+    updateRaidStatus()
+end
---
+++ raid-list.lua
@@ -0,0 +1,78 @@
+local Raids = {
+    {
+        era = "Anniversary",
+        events = {
+            {
+                name        = "Inner Turmoil",
+                available   = true,
+                lockedout   = "",
+                tooltip     = "25th Anniversary\nStarts in Katta\n/travelto katta"
+            },
+        },
+    },
+    {
+        era = "Laurion's Song",
+        events = {
+            {
+                name        = "The Return of Kanghammer",
+                available   = true,
+                lockedout   = "",
+                tooltip     = "Who: Dextris Kanghammer\nPhrase: /say this key\nZone: /travelto elddar (door 12)"
+            },
+            {
+                name        = "Hand of the King",
+                available   = true,
+                lockedout   = "",
+                tooltip     = "Who: Bidils the Quickhand\nPhrase: /say want to see\nZone: /nav loc -873 -92 23 (door 14)"
+            },
+            {
+                name        = "Shadow of My Shadow",
+                available   = true,
+                lockedout   = "",
+                tooltip     = "Who: Elistyl Kanghammer\nPhrase: /say go\nZone: /travelto timorousfalls (door 6)"
+            },
+            {
+                name        = "Prisoners of Ankexfen",
+                available   = true,
+                lockedout   = "",
+                tooltip     = "Who: Thormir Helmsbane\nPhrase: /say want\nZone: /travelto ankexfen (door 10)"
+            },
+            {
+                name        = "What is it For?",
+                available   = true,
+                lockedout   = "",
+                tooltip     = "Who: Dhakka Nogg\nPhrase: /say interested\nZone: /travelto moorsofnokk (door 9)"
+            },
+            {
+                name        = "Artisan of War",
+                available   = true,
+                lockedout   = "",
+                tooltip     = "Who: Thornel Grayleaf\nPhrase: /say wish\nZone: /travelto unkemptwoods (door 4)"
+            },
+            {
+                name        = "Heroes Are Forged",
+                available   = true,
+                lockedout   = "",
+                tooltip     = "Who: Elisel\nPhrase: /say large\nZone: /travelto herosforge"
+            },
+            {
+                name        = "Final Fugue",
+                available   = true,
+                lockedout   = "",
+                tooltip     = "Who: Shalowain\nPhrase: /say large\nZone: /travelto pallomen"
+            },
+        }
+    },
+    --[[ Add additional eras as desired
+    {
+        era = "-- to be defined --",
+        events = {
+            name        = "-- to be defined --",
+            available   = true,
+            lockedout   = "",
+            tooltip     = "Who: TBD\nPhrase: /say TBD\nZone: /travelto TBD"
+    }
+    ]]
+}
+
+return Raids
